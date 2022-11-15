8e5190b2e65b1deaeeac476ba80744f9560e4847
WowSoLaggy <dohxehapo@gmail.com>
2022-11-15 21:45:57 +0500

    (=) Extracted cubes creation to the separate file

--- LaggyDx/IShape3d.cpp
+++ LaggyDx/IShape3d.cpp
@@ -2,7 +2,6 @@
 #include "IShape3d.h"
 
 #include "Shape3d.h"
-#include "ShapesUtils.h"
 
 
 namespace Dx
@@ -60,112 +59,6 @@ namespace Dx
       return inds;
     }
 
-
-    std::vector<VertexPosNormText> generateCubeVerts(const float i_size)
-    {
-      std::vector<VertexPosNormText> verts(6 * 4);
-
-      Sdk::Vector3F normal;
-      int ind = 0;
-
-      // -X
-      normal = { -1, 0, 0 };
-      verts[ind].position = { 0, 0, 0 };
-      verts[ind++].normal = normal;
-      verts[ind].position = { 0, 0, i_size };
-      verts[ind++].normal = normal;
-      verts[ind].position = { 0, i_size, i_size };
-      verts[ind++].normal = normal;
-      verts[ind].position = { 0, i_size, 0 };
-      verts[ind++].normal = normal;
-
-      // +X
-      normal = { 1, 0, 0 };
-      verts[ind].position = { i_size, 0, i_size };
-      verts[ind++].normal = normal;
-      verts[ind].position = { i_size, 0, 0 };
-      verts[ind++].normal = normal;
-      verts[ind].position = { i_size, i_size, 0 };
-      verts[ind++].normal = normal;
-      verts[ind].position = { i_size, i_size, i_size };
-      verts[ind++].normal = normal;
-
-
-      // -Z
-      normal = { 0, 0, -1 };
-      verts[ind].position = { i_size, 0, 0 };
-      verts[ind++].normal = normal;
-      verts[ind].position = { 0, 0, 0 };
-      verts[ind++].normal = normal;
-      verts[ind].position = { 0, i_size, 0 };
-      verts[ind++].normal = normal;
-      verts[ind].position = { i_size, i_size, 0 };
-      verts[ind++].normal = normal;
-
-      // +Z
-      normal = { 0, 0, 1 };
-      verts[ind].position = { 0, 0, i_size };
-      verts[ind++].normal = normal;
-      verts[ind].position = { i_size, 0, i_size };
-      verts[ind++].normal = normal;
-      verts[ind].position = { i_size, i_size, i_size };
-      verts[ind++].normal = normal;
-      verts[ind].position = { 0, i_size, i_size };
-      verts[ind++].normal = normal;
-
-
-      // -Y
-      normal = { 0, -1, 0 };
-      verts[ind].position = { i_size, 0, 0 };
-      verts[ind++].normal = normal;
-      verts[ind].position = { i_size, 0, i_size };
-      verts[ind++].normal = normal;
-      verts[ind].position = { 0, 0, i_size };
-      verts[ind++].normal = normal;
-      verts[ind].position = { 0, 0, 0 };
-      verts[ind++].normal = normal;
-
-      // +Y
-      normal = { 0, 1, 0 };
-      verts[ind].position = { 0, i_size, 0 };
-      verts[ind++].normal = normal;
-      verts[ind].position = { 0, i_size, i_size };
-      verts[ind++].normal = normal;
-      verts[ind].position = { i_size, i_size, i_size };
-      verts[ind++].normal = normal;
-      verts[ind].position = { i_size, i_size, 0 };
-      verts[ind++].normal = normal;
-      
-      CONTRACT_ENSURE(ind == 24);
-
-      return verts;
-    }
-
-    std::vector<int> generateCubeInds()
-    {
-      std::vector<int> inds {
-        0, 1, 2,
-        0, 2, 3,
-
-        4, 5, 6,
-        4, 6, 7,
-
-        8, 9, 10,
-        8, 10, 11,
-
-        12, 13, 14,
-        12, 14, 15,
-
-        16, 17, 18,
-        16, 18, 19,
-
-        20, 21, 22,
-        20, 22, 23,
-      };
-
-      return inds;
-    }
-
   } // anonym NS
 
 
@@ -174,23 +67,6 @@ namespace Dx
   {
     auto verts = generatePlaneVerts(i_size, i_dist, i_textureCoef);
     auto inds = generatePlaneInds(i_size);
-    return std::make_unique<Shape3d>(std::move(verts), std::move(inds));
-  }
-
-
-  std::unique_ptr<IShape3d> IShape3d::cube(const float i_size)
-  {
-    auto verts = generateCubeVerts(i_size);
-    auto inds = generateCubeInds();
-    return std::make_unique<Shape3d>(std::move(verts), std::move(inds));
-  }
-
-
-  std::unique_ptr<IShape3d> IShape3d::cubeInverted(const float i_size)
-  {
-    auto verts = generateCubeVerts(i_size);
-    auto inds = generateCubeInds();
-    invertVerts(verts, inds);
     return std::make_unique<Shape3d>(std::move(verts), std::move(inds));
   }
 
---
+++ LaggyDx/IShape3d_cuboids.cpp
@@ -0,0 +1,136 @@
+#include "stdafx.h"
+#include "IShape3d.h"
+
+#include "Shape3d.h"
+#include "ShapesUtils.h"
+
+
+namespace Dx
+{
+  namespace
+  {
+    std::vector<VertexPosNormText> generateCubeVerts(const float i_size)
+    {
+      std::vector<VertexPosNormText> verts(6 * 4);
+
+      Sdk::Vector3F normal;
+      int ind = 0;
+
+      // -X
+      normal = { -1, 0, 0 };
+      verts[ind].position = { 0, 0, 0 };
+      verts[ind++].normal = normal;
+      verts[ind].position = { 0, 0, i_size };
+      verts[ind++].normal = normal;
+      verts[ind].position = { 0, i_size, i_size };
+      verts[ind++].normal = normal;
+      verts[ind].position = { 0, i_size, 0 };
+      verts[ind++].normal = normal;
+
+      // +X
+      normal = { 1, 0, 0 };
+      verts[ind].position = { i_size, 0, i_size };
+      verts[ind++].normal = normal;
+      verts[ind].position = { i_size, 0, 0 };
+      verts[ind++].normal = normal;
+      verts[ind].position = { i_size, i_size, 0 };
+      verts[ind++].normal = normal;
+      verts[ind].position = { i_size, i_size, i_size };
+      verts[ind++].normal = normal;
+
+
+      // -Z
+      normal = { 0, 0, -1 };
+      verts[ind].position = { i_size, 0, 0 };
+      verts[ind++].normal = normal;
+      verts[ind].position = { 0, 0, 0 };
+      verts[ind++].normal = normal;
+      verts[ind].position = { 0, i_size, 0 };
+      verts[ind++].normal = normal;
+      verts[ind].position = { i_size, i_size, 0 };
+      verts[ind++].normal = normal;
+
+      // +Z
+      normal = { 0, 0, 1 };
+      verts[ind].position = { 0, 0, i_size };
+      verts[ind++].normal = normal;
+      verts[ind].position = { i_size, 0, i_size };
+      verts[ind++].normal = normal;
+      verts[ind].position = { i_size, i_size, i_size };
+      verts[ind++].normal = normal;
+      verts[ind].position = { 0, i_size, i_size };
+      verts[ind++].normal = normal;
+
+
+      // -Y
+      normal = { 0, -1, 0 };
+      verts[ind].position = { i_size, 0, 0 };
+      verts[ind++].normal = normal;
+      verts[ind].position = { i_size, 0, i_size };
+      verts[ind++].normal = normal;
+      verts[ind].position = { 0, 0, i_size };
+      verts[ind++].normal = normal;
+      verts[ind].position = { 0, 0, 0 };
+      verts[ind++].normal = normal;
+
+      // +Y
+      normal = { 0, 1, 0 };
+      verts[ind].position = { 0, i_size, 0 };
+      verts[ind++].normal = normal;
+      verts[ind].position = { 0, i_size, i_size };
+      verts[ind++].normal = normal;
+      verts[ind].position = { i_size, i_size, i_size };
+      verts[ind++].normal = normal;
+      verts[ind].position = { i_size, i_size, 0 };
+      verts[ind++].normal = normal;
+
+      CONTRACT_ENSURE(ind == 24);
+
+      return verts;
+    }
+
+    std::vector<int> generateCubeInds()
+    {
+      std::vector<int> inds{
+        0, 1, 2,
+        0, 2, 3,
+
+        4, 5, 6,
+        4, 6, 7,
+
+        8, 9, 10,
+        8, 10, 11,
+
+        12, 13, 14,
+        12, 14, 15,
+
+        16, 17, 18,
+        16, 18, 19,
+
+        20, 21, 22,
+        20, 22, 23,
+      };
+
+      return inds;
+    }
+
+  } // anonym NS
+
+
+  std::unique_ptr<IShape3d> IShape3d::cube(const float i_size)
+  {
+    auto verts = generateCubeVerts(i_size);
+    auto inds = generateCubeInds();
+    return std::make_unique<Shape3d>(std::move(verts), std::move(inds));
+  }
+
+
+  std::unique_ptr<IShape3d> IShape3d::cubeInverted(const float i_size)
+  {
+    auto verts = generateCubeVerts(i_size);
+    auto inds = generateCubeInds();
+    invertVerts(verts, inds);
+    return std::make_unique<Shape3d>(std::move(verts), std::move(inds));
+  }
+
+} // ns Dx
